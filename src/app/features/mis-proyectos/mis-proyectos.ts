08a7b9eefbd4b8601ecebc09709ce7e4eeb50fb7
Ara <aragomezgd@gmail.com>
2025-11-09 10:19:03 -0300

    Primer commit

---
+++ src/app/features/mis-proyectos/mis-proyectos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-proyectos',
+  imports: [],
+  templateUrl: './mis-proyectos.html',
+  styleUrl: './mis-proyectos.css',
+})
+export class MisProyectos {
+
+}
